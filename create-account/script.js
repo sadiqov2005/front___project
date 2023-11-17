70ae867488b32273ee09ad6fc3b01a1999da0e0d
isasadiqov0401~ <isays@code.edu.az>
2023-11-17 12:08:50 +0400

    validations in home and create-account page uptated

--- create-account/script.js
+++ create-account/script.js
@@ -8,6 +8,59 @@ const signUpBtn=document.querySelector('.main-nav>ul>li:nth-child(4)');
 const createAccountBtn = document.querySelector('#create-account-btn');
 const formCreateAccount = document.getElementById('form-create-account');
 const logoNFT=document.querySelector('.main-header > .logo');
+const subscribeFooterBtn = document.querySelector('.subscribe-footer form button');
+const subscribeFooterInput=document.querySelector('.subscribe-footer input');
+
+
+
+
+
+
+
+subscribeFooterBtn.addEventListener('click', (e) => {
+e.preventDefault();
+console.log("click");
+
+const emailFooter = subscribeFooterInput.value;
+
+function checkEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+    if (isValid) {
+        console.log("email is valid");
+    } else {
+        console.log("email is INVALID");
+    }
+    return isValid;
+}
+
+const isEmailValidFooter = checkEmail(emailFooter);
+
+if (isEmailValidFooter) {
+    showToast("Success! Form is valid.", "linear-gradient(to right, #00b09b, #96c93d)");
+} else {
+    showToast("Invalid input. Please check your details.", "linear-gradient(to right, #e74c3c, #c0392b)");
+}
+});
+
+
+
+function showToast(message, background) {
+Toastify({
+    text: message,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+        background: background,
+    },
+    onClick: function () {} 
+}).showToast();
+}
 
 
 
@@ -57,6 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
 createAccountBtn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('clicked');
@@ -67,35 +121,32 @@ createAccountBtn.addEventListener('click', (e) => {
     const confirmPassword = inputConfirmPassword.value;
 
     function checkUsername(username) {
-        a= username.length >= 4
-        if (a==true){
+        a = username.length >= 4;
+        if (a) {
             console.log("Username is valid");
-        }
-        else {
+        } else {
             console.log("Username is INVALID");
         }
         return a;
     }
 
     function checkEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        b = emailRegex.test(email)
-        if (b==true){
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        b = emailRegex.test(email);
+        if (b) {
             console.log("email is valid");
-        }
-        else {
+        } else {
             console.log("email is INVALID");
         }
         return b;
     }
 
     function checkPassword(password) {
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-        c= passwordRegex.test(password)
-        if (c==true){
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+        c = passwordRegex.test(password);
+        if (c) {
             console.log("pass is valid");
-        }
-        else {
+        } else {
             console.log("pass is INVALID");
         }
         return c;
@@ -106,19 +157,21 @@ createAccountBtn.addEventListener('click', (e) => {
     const isPasswordValid = checkPassword(password);
 
     if (isUsernameValid && isEmailValid && isPasswordValid && password === confirmPassword) {
-        showToast("Success! Form is valid.");
+        showToast("Success! Form is valid.", "linear-gradient(to right,#2ecc71, #27ae60)");
     } else {
         showToast("Invalid input. Please check your details.");
     }
 });
 
-function showToast(message) {
+function showToast(message, background) {
     Toastify({
         text: message,
-        duration: 3000,  
-        gravity: "top", 
-        close: true,    
-        backgroundColor: "RED", 
+        duration: 3000,
+        gravity: "top",
+        close: true,
+        style: {
+            background: background,
+        },
     }).showToast();
 }
 
--- home-page/script.js
+++ home-page/script.js
@@ -1,25 +1,5 @@
 
-    // const nft_logo=document.querySelector(".main-header>.logo");
-    // var emailInput=document.querySelector('.subcribe-form input');
-    // const subscribeBtn=document.querySelector('.subcribe-form button');
 
-
-    // nft_logo.addEventListener('click', () => {
-    //     window.location.href = '../home-page/index.html'; 
-    // });
-
-
-
-    // function checkEmail(email) {
-    //     // Regular expression for basic email validation
-    //     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    //     return emailRegex.test(email);
-    // }
-
-
-    // subscribeBtn.addEventListener('click',()=>{
-    //     var emailValue=emailInput.value;
-    // })
 
     const marketplaceElement=document.querySelector('.main-nav>ul>li:first-child');
     const rankingsElement=document.querySelector('.main-nav>ul>li:nth-child(2)');
@@ -28,63 +8,87 @@
     const viewRankingsBtn=document.querySelector(".Top-creators button");
     const subscribeBtnWidget=document.querySelector(".subscribe-widget-container #subscribe-button");
     const subcribeBtnFooter=document.querySelector(".subscribe-footer form button");
-    const inputEmailWidget=document.querySelector(".subscribe-widget-container input");
+    const inputEmailWidget=document.getElementById("inputEmailWidget");
+    const getStartedBtn=document.querySelector(".left-columnInHeroFrame>.row3>button");
+    const subscribeFooterBtn = document.querySelector('.subscribe-footer form button');
+    const subscribeFooterInput=document.querySelector('.subscribe-footer input');
 
 
+   // validations
 
-    // subscribeBtnWidget.addEventListener("click",(e)=>{
-    //     e.preventDefault();
-    //     console.log("clickedf");
-    // })
+   
 
-    // subcribeBtnFooter.addEventListener("click",(e)=>{
-    //     e.preventDefault();
+   subscribeBtnWidget.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('clicked');
 
-    //     console.log("demeli");
-    // })
+    const email = inputEmailWidget.value;
 
-
-
-    subscribeBtnWidget.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log('clicked');
-
-        const email = inputEmailWidget.value;
-
-
-
-        function checkEmail(email) {
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            b = emailRegex.test(email)
-            if (b==true){
-                console.log("email is valid");
-            }
-            else {
-                console.log("email is INVALID");
-            }
-            return b;
-        }
-
-
-        const isEmailValid = checkEmail(email);
-
-        if (isEmailValid ) {
-            showToast("Success! Form is valid.");
+    function checkEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValid = emailRegex.test(email);
+        if (isValid) {
+            console.log("email is valid");
         } else {
-            showToast("Invalid input. Please check your details.");
+            console.log("email is INVALID");
         }
-    });
-    function showToast(message) {
-        Toastify({
-            text: message,
-            duration: 3000,  
-            gravity: "top", 
-            close: true,    
-            style: {
-                backgroundColor: "green",  // Corrected syntax
-            },
-        }).showToast();
+        return isValid;
     }
+
+    const isEmailValid = checkEmail(email);
+
+    if (isEmailValid) {
+        showToast("Success! Form is valid.", "linear-gradient(to right, #00b09b, #96c93d)");
+    } else {
+        showToast("Invalid input. Please check your details.", "linear-gradient(to right, #e74c3c, #c0392b)");
+    }
+});
+
+subscribeFooterBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("click");
+
+    const emailFooter = subscribeFooterInput.value;
+
+    function checkEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValid = emailRegex.test(email);
+        if (isValid) {
+            console.log("email is valid");
+        } else {
+            console.log("email is INVALID");
+        }
+        return isValid;
+    }
+
+    const isEmailValidFooter = checkEmail(emailFooter);
+
+    if (isEmailValidFooter) {
+        showToast("Success! Form is valid.", "linear-gradient(to right, #00b09b, #96c93d)");
+    } else {
+        showToast("Invalid input. Please check your details.", "linear-gradient(to right, #e74c3c, #c0392b)");
+    }
+});
+
+
+
+function showToast(message, background) {
+    Toastify({
+        text: message,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: background,
+        },
+        onClick: function () {} 
+    }).showToast();
+}
+
     
 
 
@@ -94,9 +98,9 @@
 
 
 
-
-
-
+    
+    
+    // go to another page of NFT
 
 function openMarketplace(){
     marketplaceElement.addEventListener("click",()=>{
@@ -109,6 +113,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function openSignCreateAccounPage(){
     signUpBtn.addEventListener("click",()=>{
+        window.location.href="../create-account/index.html"
+    })
+    getStartedBtn.addEventListener("click",()=>{
         window.location.href="../create-account/index.html"
     })
 }
